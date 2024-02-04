fcc3cdb84884196c76893206c3064493d0beff38
AiSonu-patel <aisonupatel09@gmail.com>
2024-02-04 22:06:53 +0530

    add

---
+++ String all/PrintAllSubstring.java
@@ -0,0 +1,14 @@
+/**
+ * PrintAllSubstring
+ */
+public class PrintAllSubstring {
+
+    public static void main(String[] args) {
+        String str = "abcd";
+        for(int i = 0; i <= 3; i++){
+            for(int j = i+1; j <= 4; j++){
+                System.out.print(str.substring(i, j)+" ");
+            }
+        }
+    }
+}

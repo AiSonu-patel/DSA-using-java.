845e38988ba23a337b1234911968287230d78156
AiSonu-patel <aisonupatel09@gmail.com>
2024-03-10 00:17:18 +0530

    Add

---
+++ Stacks Java/NerestGreaterElement.java
@@ -0,0 +1,72 @@
+import java.util.Stack;
+
+public class NerestGreaterElement {
+    public static int[] NextGreaterToRight(int[] arr, int n){
+        int[] ans = new int[n];
+        Stack<Integer> st = new Stack<>();
+        for(int i=n-1; i>=0; i--){
+            if(st.size()==0){
+                ans[i] = -1;
+            }
+            else if(st.size()>0 && arr[i]<st.peek()){
+                ans[i] = st.peek();
+            }
+            else if(st.size()>0 && arr[i]>=st.peek()){
+                while(st.size()>0 && arr[i]>=st.peek()){
+                    st.pop();
+                }
+                if(st.size()==0){
+                    ans[i] = -1;
+                }
+                else{
+                    ans[i] = st.peek();
+                }
+            }
+            st.push(arr[i]);
+        }
+        return ans;
+    }
+
+    public static int[] NextgreaterToLeft(int[] arr, int n){
+        int[] ans = new int[n];
+        Stack<Integer> st = new Stack<>();
+        for(int i=0; i<n; i++){
+            if(st.size()==0){
+                ans[i] = -1;
+            }
+            else if(st.size()>0 && st.peek()>arr[i]){
+                ans[i] = st.peek();
+            }
+            else if(st.size()>0 && st.peek()<=arr[i]){
+                while(st.size()>0 && st.peek()<=arr[i]){
+                    st.pop();
+                }
+                if(st.size()==0){
+                    ans[i] = -1;
+                }
+                else{
+                    ans[i] = st.peek();
+                }
+            }
+            st.push(arr[i]);
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        int[] arr = {4,8,5,2,3,6,9,1};
+        int n = arr.length;
+        for(int i=0; i<n; i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+        int[] ans = NextGreaterToRight(arr, n);
+        for(int i=0; i<n; i++){
+            System.out.print(ans[i]+" ");
+        }
+        System.out.println();
+        int[] ans2 = NextgreaterToLeft(arr, n);
+        for(int i=0; i<n; i++){
+            System.out.print(ans2[i]+" ");
+        }
+    }
+}
